4dee33c8fe59ffe792519033c855e2da82f7a1db
zx576 <782744680@qq.com>
2017-08-10 16:17:23 +0800

    <deploy>

--- recruitment/wsgi.py
+++ recruitment/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recruitment.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recruitment.server_settings")
 
 application = get_wsgi_application()
